da227ae8a71af727ec4b2e7cbad6888884555184
Dushime Bill Benon <DushimeBillBenon@gmail.com>
2023-11-30 17:06:46 +0200

    init: made a public version

---
+++ listings/choices.py
@@ -0,0 +1,60 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': '100000',
+    '200000': '200000',
+    '300000': '300000',
+    '400000': '400000',
+    '500000': '500000',
+    '800000': '800000',
+    '1200000': '1200000',
+    '1500000': '1500000',
+    '2000000': '2000000',
+    '10000000': '10M+',
+}
+
+district_choices = {
+    'Kicukiro': 'Kicukiro',
+    'Gasabo': 'Gasabo',
+    'Nyarugenge': 'Nyarugenge',
+    'Nyagatare': 'Nyagatare',
+    'Rwamagana': 'Rwamagana',
+    'Kayonza': 'Kayonza',
+    'Bugesera': 'Bugesera',
+    'Huye': 'Huye',
+    'Nyanza': 'Nyanza',
+    'Kamonyi': 'Kamonyi',
+    'Rusizi': 'Rusizi',
+    'Rubavu': 'Rubavu',
+    'Karongi': 'Karongi',
+    'Musanze': 'Musanze',
+}
+
+
+city_choices = {
+    'Kigali': 'Kigali',
+    'Bugesera': 'Bugesera',
+    'Nyagatare': 'Nyagatare',
+    'Rwamagana': 'Rwamagana',
+    'Kayonza': 'Kayonza',
+    'Bugesera': 'Bugesera',
+    'Huye': 'Huye',
+    'Nyanza': 'Nyanza',
+    'Kamonyi': 'Kamonyi',
+    'Rusizi': 'Rusizi',
+    'Rubavu': 'Rubavu',
+    'Karongi': 'Karongi',
+    'Musanze': 'Musanze',
+    'Muhanga': 'Muhanga'
+}
